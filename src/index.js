c27d8ee3f51470ac9d7f0c01f96ca214f4400dff
Inasalamov Amirkhan <104070261+Vedanho@users.noreply.github.com>
2024-05-21 14:54:13 +0300

    added word processing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-const search = (docs, str) => docs.filter(({ text }) => text.split(' ').some((word) => word === str));
+const search = (docs, str) => {
+  const sanitize = (text) => text.replace(/[^\w\s]/g, '').toLowerCase();
+
+  const sanitizedStr = sanitize(str);
+  return docs.filter(({ text }) => text.split(' ').some((word) => sanitize(word) === sanitizedStr));
+};
 
 export default search;
